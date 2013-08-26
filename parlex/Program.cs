38d1e4666bb5e2294c6afdae440b8337fff40f65
Brent Lewis <coder0xff@gmail.com>
2013-08-26 08:20:35 -0700

    Ticket: Get rid of the trailing relations thing https://app.asana.com/0/6407974508414/6646909471147 Add a Program.Main to parlex library

---
+++ parlex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace parlex {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            string testFile = File.ReadAllText(@"C:\Users\Brent\Dropbox\parlex\test.ple");
+            var grammarDocument = GrammarDocument.FromString(testFile);
+            var compiledGrammar = new CompiledGrammar(grammarDocument);
+            string toParseFile = File.ReadAllText(@"C:\Users\Brent\Dropbox\parlex\parse_test.txt");
+            var parseResult = Parser.Parse(toParseFile, "document", compiledGrammar);
+            System.Diagnostics.Debug.WriteLine(parseResult.Product.Title);
+        }
+    }
+}
