86d7cb5fd5d8102a0bffcf5b5a82698ea7bc8d57
Brent Lewis <coder0xff@gmail.com>
2013-08-26 08:47:27 -0700

    Some changes that weren't saved

--- UnitTestProject1/GrammarTest.cs
+++ UnitTestProject1/GrammarTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using parlex;
+using System.IO;
 
 namespace UnitTestProject1 {
     [TestClass]
