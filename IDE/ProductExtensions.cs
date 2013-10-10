60d269efa7104a205ff1bbb27b6d2680c637d8d0
Brent Lewis <coder0xff@gmail.com>
2013-10-10 11:07:50 -0700

    Always minimize an Nfa before loading it into the editor, and make the graph drawing prettier.

--- IDE/ProductExtensions.cs
+++ IDE/ProductExtensions.cs
@@ -8,7 +8,7 @@ using parlex;
 namespace IDE {
     public static class ProductExtensions {
         public static Nfa<Product, int> ToNfa(this Product product) {
-            return Nfa<Product, int>.Union(product.Sequences.Select(sequence => sequence.ToNfa()));
+            return Nfa<Product, int>.Union(product.Sequences.Select(sequence => sequence.ToNfa())).Minimized();
         }
 
         public static GrammarDocument ToGrammarDocument(this Product product, Dictionary<String, Product> allProducts) {
