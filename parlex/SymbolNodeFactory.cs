4725c192bda761b3b1312e980a319ae314831900
Brent Lewis <coder0xff@gmail.com>
2015-03-14 11:52:21 -0400

    Equality test for SymbolNodeFactory

--- parlex/SymbolNodeFactory.cs
+++ parlex/SymbolNodeFactory.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace Parlex {
     internal class SymbolNodeFactory : ISyntaxNodeFactory {
+        protected bool Equals(SymbolNodeFactory other) {
+            return Equals(_production, other._production) && Equals(_terminal, other._terminal);
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                return ((_production != null ? _production.GetHashCode() : 0)*397) ^ (_terminal != null ? _terminal.GetHashCode() : 0);
+            }
+        }
+
         private readonly Grammar.Production _production;
         private readonly Grammar.ITerminal _terminal;
 
@@ -52,6 +62,19 @@ namespace Parlex {
 
         public override string ToString() {
             return Name;
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) {
+                return false;
+            }
+            if (ReferenceEquals(this, obj)) {
+                return true;
+            }
+            if (obj.GetType() != this.GetType()) {
+                return false;
+            }
+            return Equals((SymbolNodeFactory)obj);
         }
     }
 }
