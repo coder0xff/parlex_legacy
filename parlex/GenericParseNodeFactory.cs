5b78e789abaacf7881ab399b8f494381a9317dda
Brent Lewis <coder0xff@gmail.com>
2015-04-09 01:36:53 -0400

    GenericParseNodeFactory can activate behavior similar to "Recognizer architecture", which is a tricky refactor/replacement for ParseNode. By toggling some code comments, GenericParseNodeFactory only creates one ParseNode instance for each Type - which is the goal of refactoring ParseNode to Recognizer.

--- parlex/GenericParseNodeFactory.cs
+++ parlex/GenericParseNodeFactory.cs
@@ -1,90 +1,50 @@
 ﻿using System;
+using System.Collections.Concurrent.More;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Parlex {
-    public class GenericParseNodeFactory<T> : IParseNodeFactory where T : ParseNode, new() {
-        public string Name { get; private set; }
-        public bool IsGreedy { get; private set; }
-
-        ParseNode IParseNodeFactory.Create() {
-            return new T();
-        }
-
-        bool IParseNodeFactory.Is(TerminalDefinition terminalDefinition) {
-            return false;
-        }
-
-        bool IParseNodeFactory.Is(NfaProduction production) {
-            return false;
-        }
-
-        public GenericParseNodeFactory() {
-            Name = typeof(T).Name;
-            IsGreedy = typeof(T).GetCustomAttributes(typeof(GreedyAttribute), false).Length > 0;
-        }
-
-        public static IParseNodeFactory FromType(Type t) {
-            var t2 = typeof(GenericParseNodeFactory<>).MakeGenericType(t);
-            return (IParseNodeFactory)Activator.CreateInstance(t2);
-        }
-
-        public override bool Equals(object obj) {
-            if (ReferenceEquals(null, obj)) {
-                return false;
-            }
-            if (ReferenceEquals(this, obj)) {
-                return true;
-            }
-            var asNonGeneric = obj as GenericParseNodeFactory;
-            if (asNonGeneric != null) {
-                return Equals(asNonGeneric._backing);
-            }
-            if (obj.GetType() != GetType()) {
-                return false;
-            }
-            return true;
-        }
-
-        public override int GetHashCode() {
-            return typeof(T).GetHashCode();
+    public class GenericParseNodeFactory<T> : GenericParseNodeFactory, IParseNodeFactory where T : ParseNode, new() {
+        public GenericParseNodeFactory() : base(typeof(T)) {
         }
     }
 
     public class GenericParseNodeFactory : IParseNodeFactory {
-        private Type _t;
-        internal IParseNodeFactory _backing;
+        private static AutoDictionary<Type, ParseNode> nodes;
+        static GenericParseNodeFactory() {
+            nodes = new AutoDictionary<Type, ParseNode>(t => (ParseNode)Activator.CreateInstance(t));
+        }
+
+        private readonly Type _t;
+        private String _name;
+        private bool _isGreedy;
 
         public GenericParseNodeFactory(Type t) {
             _t = t;
-            var t2 = typeof(GenericParseNodeFactory<>).MakeGenericType(t);
-            _backing = (IParseNodeFactory)Activator.CreateInstance(t2);
+            _name = _t.Name;
+            _isGreedy = _t.GetCustomAttributes(typeof(GreedyAttribute), false).Length > 0;
         }
 
         string IParseNodeFactory.Name {
-            get { return _backing.Name; }
+            get { return _name; }
         }
 
         bool IParseNodeFactory.IsGreedy {
-            get { return _backing.IsGreedy; }
+            get { return _isGreedy; }
         }
 
         ParseNode IParseNodeFactory.Create() {
-            return _backing.Create();
+            return (ParseNode)Activator.CreateInstance(_t); //nodes[_t];
         }
 
         bool IParseNodeFactory.Is(TerminalDefinition terminalDefinition) {
-            return _backing.Is(terminalDefinition);
+            return false;
         }
 
         bool IParseNodeFactory.Is(NfaProduction production) {
-            return _backing.Is(production);
-        }
-
-        public override bool Equals(object obj) {
-            return _backing.Equals(obj);
+            return false;
         }
 
         public override int GetHashCode() {
